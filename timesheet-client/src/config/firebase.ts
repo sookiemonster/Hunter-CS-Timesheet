87fc37b492d0d2ec1eed540134db0378017c4fd9
juliannea <julianneaguilar74@gmail.com>
2024-12-04 19:35:01 -0500

    google firebase login works!

--- timesheet-client/src/config/firebase.ts
+++ timesheet-client/src/config/firebase.ts
@@ -1,8 +1,8 @@
 //@ts-nocheck
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"; 
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
--- timesheet-client/src/pages/Login.tsx
+++ timesheet-client/src/pages/Login.tsx
@@ -1,4 +1,21 @@
 //@ts-nocheck
+import {auth, provider} from '../config/firebase'
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+
 export const Login = () =>{
-  return <h1> Login Page </h1>;
+  const navigate = useNavigate(); //allows to navigate from one page to another 
+  const signInWithGoogle = async() =>{
+   
+    //return a promise 
+    const result = await signInWithPopup(auth, provider); 
+    console.log(result);
+    navigate('/Home');
+  };
+
+  return <div>
+    <p>Sign In With Google</p>
+    <button onClick={signInWithGoogle}>Sign in With Google</button>
+  </div>;
 };
