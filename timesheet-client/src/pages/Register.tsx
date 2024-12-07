3b6287ff2f06f2b8396661e638ff40b09c2f5ced
juliannea <julianneaguilar74@gmail.com>
2024-12-07 12:57:18 -0500

    add user role in collection

--- timesheet-client/src/pages/Register.tsx
+++ timesheet-client/src/pages/Register.tsx
@@ -1,10 +1,8 @@
 //@ts-nocheck
-import {auth, provider} from '../config/firebase'
-import { signInWithPopup, 
-  sendEmailVerification, 
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-  createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth, provider, db} from '../config/firebase'
+import {  sendEmailVerification, } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
+
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useContext } from "react";
@@ -18,12 +16,22 @@ export const Register = () =>{
   const [password, setPassword] = useState('')
   const [registrationStatus, setRegistrationStatus] = useState(null);
 
+  const roleRef = collection(db, "user"); //reference to user collection
+
   const register = async (e) => {
     e.preventDefault()
     console.log("register function called")
     try{
       const result = await doCreateUserWithEmailAndPassword(email, password);
       const user = result.user
+
+      //register as ta 
+      await addDoc(roleRef, {
+        email: user.email,
+        id: user.uid,
+        role: "ta"
+      })
+
       console.log("verification status: ", user.emailVerified)
       if(!user.emailVerified){
         await sendEmailVerification(user);
