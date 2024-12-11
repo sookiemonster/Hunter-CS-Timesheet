b968046db0d4e8911e26ab32382912bfb977b804
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2024-12-11 16:02:17 -0500

    Add retries to useFetchLocal

--- timesheet-client/src/state/hooks/useFetchLocal.tsx
+++ timesheet-client/src/state/hooks/useFetchLocal.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useFetch } from "@mantine/hooks";
 
 
 export default function useFetchLocal<T>(endpoint:string) {
+    const attempts = useRef(3);
     const { data, loading, error, refetch, abort } = useFetch<T>(
         `http://localhost:8000${endpoint}`
     );
+
+    useEffect(() => {
+        if (attempts.current < 0 ) { abort(); return; }
+        if (error) { 
+            setTimeout(() => {
+                attempts.current = attempts.current - 1; 
+                refetch();
+            }, 5000)
+        }
+    }, [error, data, loading])
 
     return { data, loading, error, refetch, abort };
 }
