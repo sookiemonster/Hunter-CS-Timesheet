e5381487ba5423cd625f2e5d25725db48618e0cf
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2024-12-11 15:55:21 -0500

    Whoops undo CORs fix

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(express.json());
 
 app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
